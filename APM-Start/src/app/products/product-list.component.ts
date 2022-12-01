34edc7eb32a52803ea5ee1d6d104e7310c94d6c4
trystanclarke <trystan.clarke@outlook.com>
2022-12-01 16:52:14 +0000

    Created product service and moved hardcoded products

--- APM-Start/src/app/products/product-list.component.ts
+++ APM-Start/src/app/products/product-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ProductService } from './product.service';
 import { IProduct } from './products';
 
 @Component({
@@ -28,41 +29,14 @@ export class ProductListComponent implements OnInit {
 
   filteredProducts: IProduct[] = [];
 
-  products: IProduct[] = [
-    {
-      productId: 1,
-      productName: 'Leaf Rake',
-      productCode: 'GDN-0011',
-      releaseDate: 'March 19, 2021',
-      description: 'Leaf rake with 48-inch wooden handle.',
-      price: 19.95,
-      starRating: 3.2,
-      imageUrl: 'assets/images/leaf_rake.png',
-    },
-    {
-      productId: 2,
-      productName: 'Garden Cart',
-      productCode: 'GDN-0023',
-      releaseDate: 'March 18, 2021',
-      description: '15 gallon capacity rolling garden cart',
-      price: 32.99,
-      starRating: 4.2,
-      imageUrl: 'assets/images/garden_cart.png',
-    },
-    {
-      productId: 5,
-      productName: 'Hammer',
-      productCode: 'TBX-0048',
-      releaseDate: 'May 21, 2021',
-      description: 'Curved claw steel hammer',
-      price: 8.9,
-      starRating: 4.8,
-      imageUrl: 'assets/images/hammer.png',
-    },
-  ];
+  products: IProduct[] = [];
+
+  constructor(private productService: ProductService) {}
 
   ngOnInit(): void {
     console.log('ngOnInit');
+    this.products = this.productService.getProducts();
+    this.filteredProducts = this.products;
     this.listFilter = '';
   }
 
---
+++ APM-Start/src/app/products/product.service.ts
@@ -0,0 +1,42 @@
+import { Injectable } from '@angular/core';
+import { IProduct } from './products';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ProductService {
+  getProducts(): IProduct[] {
+    return [
+      {
+        productId: 5,
+        productName: 'Hammer',
+        productCode: 'TBX-0048',
+        releaseDate: 'May 21, 2021',
+        description: 'Curved claw steel hammer',
+        price: 8.9,
+        starRating: 4.8,
+        imageUrl: 'assets/images/hammer.png',
+      },
+      {
+        productId: 8,
+        productName: 'Saw',
+        productCode: 'TBX-0022',
+        releaseDate: 'May 15, 2021',
+        description: '15-inch steel blade hand saw',
+        price: 11.55,
+        starRating: 3.7,
+        imageUrl: 'assets/images/saw.png',
+      },
+      {
+        productId: 10,
+        productName: 'Video Game Controller',
+        productCode: 'GMG-0042',
+        releaseDate: 'October 15, 2020',
+        description: 'Standard two-button video game controller',
+        price: 35.95,
+        starRating: 4.6,
+        imageUrl: 'assets/images/xbox-controller.png',
+      },
+    ];
+  }
+}
