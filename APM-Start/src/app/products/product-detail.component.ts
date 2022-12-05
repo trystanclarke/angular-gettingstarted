4e12235cee7652ba4380313a1598170de403ffa8
trystanclarke <trystan.clarke@outlook.com>
2022-12-05 17:43:13 +0000

    Added routing to product detail page

--- APM-Start/src/app/products/product-detail.component.ts
+++ APM-Start/src/app/products/product-detail.component.ts
@@ -1,12 +1,37 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ProductService } from './product.service';
+import { IProduct } from './products';
 
 @Component({
   templateUrl: './product-detail.component.html',
   styleUrls: ['./product-detail.component.css'],
 })
 export class ProductDetailComponent implements OnInit {
-  pageTitle = 'Product Detail';
-  constructor() {}
+  get pageTitle() {
+    return 'Product Detail: ' + this.product?.productName;
+  }
 
-  ngOnInit(): void {}
+  product: IProduct | undefined;
+
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private productService: ProductService
+  ) {}
+
+  ngOnInit(): void {
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+
+    this.productService.getProducts().subscribe({
+      next: (products) => {
+        const product = products.find((p) => p.productId === id);
+        this.product = product;
+      },
+    });
+  }
+
+  onBack(): void {
+    this.router.navigate(['/products']);
+  }
 }
